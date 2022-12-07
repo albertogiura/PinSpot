d42c3c3035f1903d2641229d188ce0272dff33e5
Simone-Tombolini <simone.tombolini@gmail.com>
2022-12-08 00:15:24 +0100

    aggiunte di prova
    blue dot e prova search bar

--- MyFragmentApp/app/src/main/java/com/example/myfragmentapp/MapFragment.java
+++ MyFragmentApp/app/src/main/java/com/example/myfragmentapp/MapFragment.java
@@ -1,17 +1,34 @@
 package com.example.myfragmentapp;
 
+import static android.content.ContentValues.TAG;
+
+import static androidx.core.content.ContextCompat.getSystemService;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
@@ -20,6 +37,12 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnSuccessListener;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -32,6 +55,12 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, GoogleM
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    private ActivityResultLauncher<String> singlePermissionLauncher;
+    private ActivityResultContracts.RequestPermission singlePermissionContract;
+
+    private FusedLocationProviderClient fusedLocationClient;
+    public LatLng mypos = new LatLng(0,0);
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -72,25 +101,72 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, GoogleM
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity());
+        singlePermissionContract = new ActivityResultContracts.RequestPermission();
+        //multiplePermissionsContract = new ActivityResultContracts.RequestMultiplePermissions();
+
+        singlePermissionLauncher = registerForActivityResult(singlePermissionContract, isGranted -> {
+            if (isGranted) {
+                // Permission is granted. Continue the action or workflow in your app.
+                Log.d(TAG, "Single permission has been granted");
+
+            } else {
+                // Explain to the user that the feature is unavailable because the feature requires
+                // a permission that the user has denied. At the same time, respect
+                // the user's decision. Don't link to system settings in an effort to convince the
+                // user to change their decision.
+                Log.d(TAG, "Single permission has not been granted");
+            }
+        });
+
         return inflater.inflate(R.layout.fragment_map, container, false);
+
     }
+
+    //public LatLng myPos;//la mia posizione
+    //necessari per il permesso
+
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext());
 
+        singlePermissionContract = new ActivityResultContracts.RequestPermission();
         MapView mapView = view.findViewById(R.id.mapv);
         mapView.onCreate(savedInstanceState);
+
         mapView.onResume();
         mapView.getMapAsync(this);
+
+
     }
 
-    private Marker marker;
+    private Marker marker;//marker di test
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         LatLng milano = new LatLng(45, 9);
 
+        googleMap.setMaxZoomPreference(20.0f);
+        googleMap.setMinZoomPreference(14.0f);
+
+        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            getLocation(googleMap);
+            return;
+        }
+        // Turn on the My Location layer and the related control on the map.
+
+        getLocation(googleMap);
+        googleMap.setMyLocationEnabled(true);
         marker = googleMap.addMarker(new MarkerOptions()
                 .position(milano)
                 .title("Marker")
@@ -101,9 +177,14 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, GoogleM
         );
         marker.setTag(0);
         googleMap.setOnMarkerClickListener(this); //importante settare il listener NON DIMENTICARE
+       /* Location myLocation = googleMap.getMyLocation();//è deprecato, chiedere a ginelli se va bene
+       LatLng myLatLng = new LatLng(myLocation.getLatitude(),
+              myLocation.getLongitude());
+       */ //googleMap.moveCamera(CameraUpdateFactory.newLatLng(milano));
+        getLocation(googleMap);
 
-        //googleMap.moveCamera(CameraUpdateFactory.newLatLng(milano));
-        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(milano, 20.0f));//per fare zoom con animazione
+
+      //per fare zoom con animazione
     }
 
     @Override
@@ -125,6 +206,46 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, GoogleM
         // Return false to indicate that we have not consumed the event and that we wish
         // for the default behavior to occur (which is for the camera to move such that the
         // marker is centered and for the marker's info window to open, if it has one).
+
         return false;
+    }
+    private void getLocation(GoogleMap map) {
+
+        boolean singlePermissionsStatus =
+                ActivityCompat.checkSelfPermission(requireContext(),
+                        Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+/*
+        boolean multiplePermissionsStatus =
+                ActivityCompat.checkSelfPermission(this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                        ActivityCompat.checkSelfPermission(this,
+                                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+*/
+        // Use multiplePermissionsStatus if you want to try the logic
+        // of requesting more than one permission
+        if (singlePermissionsStatus) {
+            Log.d(TAG, "getLocation(): All permissions have been granted");
+            // Doc is here: https://developer.android.com/training/location/retrieve-current
+            fusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(requireActivity(),  new OnSuccessListener<Location>() {
+                        public void onSuccess(Location location) {
+                            if (location != null) {
+                                Log.d(TAG, location.getLatitude() + " " + location.getLongitude());
+                                LatLng currentPos = new LatLng(location.getLatitude(), location.getLongitude());
+
+                                mypos = currentPos;
+                                map.animateCamera(CameraUpdateFactory.newLatLngZoom(mypos, 14.0f));
+                            }
+                        }
+                    });
+
+        } else {
+            Log.d(TAG, "One or more permissions have not been granted");
+            // Use multiplePermissionLauncher and PERMISSIONS variable as argument of method launch
+            // if you want to try the logic of requesting more than one permission
+            singlePermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION);
+            //multiplePermissionLauncher.launch(PERMISSIONS);
+        }
+
     }
 }
