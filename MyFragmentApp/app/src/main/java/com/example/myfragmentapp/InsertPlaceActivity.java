ccdeae128eafd9ca8a6b288fecedf234aa4e4e61
Gambamat <m.gamba12@campus.unimib.it>
2022-12-27 14:14:27 +0100

    scegli da galleria

--- MyFragmentApp/app/src/main/java/com/example/myfragmentapp/InsertPlaceActivity.java
+++ MyFragmentApp/app/src/main/java/com/example/myfragmentapp/InsertPlaceActivity.java
@@ -1,11 +1,15 @@
 package com.example.myfragmentapp;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Intent;
@@ -20,19 +24,23 @@ import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.Objects;
 
 
 public class InsertPlaceActivity extends AppCompatActivity {
 
-
     private Button button2;
+    private Button button3;
     private Button downloadButton;
     private ImageView image;
     private static final int REQUEST_CODE=1;
+    private static final int Read_Permission=101;
+    private static final int SELECT_IMAGE=100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,20 +48,37 @@ public class InsertPlaceActivity extends AppCompatActivity {
         setContentView(R.layout.activity_insertplace);
         Bundle extras = getIntent().getExtras();
         byte[] byteArray = extras.getByteArray("picture");
+        downloadButton=findViewById(R.id.buttonDownload);
 
+       if(ContextCompat.checkSelfPermission(InsertPlaceActivity.this,Manifest.permission.READ_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(InsertPlaceActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},Read_Permission);
+
+        }
+        button3=findViewById(R.id.gallery);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(
+                        Intent.createChooser(intent,"Select Image"),
+                        SELECT_IMAGE
+                );
+            }
+        });
         Bitmap bmp = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
         image = (ImageView) findViewById(R.id.myImage);
         image.setImageBitmap(bmp);
         button2=findViewById(R.id.buttonHome);
-        downloadButton=findViewById(R.id.buttonDownload);
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent openHome = new Intent(InsertPlaceActivity.this, MainActivity.class);
-
                 startActivity(openHome);
             }
         });
+
         downloadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -70,6 +95,8 @@ public class InsertPlaceActivity extends AppCompatActivity {
                 }
             }
         });
+
+
     }
 
     @Override
@@ -79,7 +106,7 @@ public class InsertPlaceActivity extends AppCompatActivity {
             if(grantResults.length>0 && grantResults[0]== PackageManager.PERMISSION_GRANTED){
                 saveImage();
             }else{
-                Toast.makeText(this, "Please provide required permission", Toast.LENGTH_SHORT).show();
+                Toast.makeText(InsertPlaceActivity.this, "Please provide required permission", Toast.LENGTH_SHORT).show();
             }
         }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -106,10 +133,10 @@ public class InsertPlaceActivity extends AppCompatActivity {
             Bitmap bitmap=bitmapDrawable.getBitmap();
 
             OutputStream outputStream= contentResolver.openOutputStream(Objects.requireNonNull(uri));
-            bitmap.compress(Bitmap.CompressFormat.JPEG,1000,outputStream);
+            bitmap.compress(Bitmap.CompressFormat.JPEG,100,outputStream);
             Objects.requireNonNull(outputStream);
 
-            Toast.makeText(this, "Images Saved Succesfully", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Images Saved Successfully", Toast.LENGTH_SHORT).show();
 
         }catch (Exception e){
             Toast.makeText(this, "Images not Saved ", Toast.LENGTH_SHORT).show();
@@ -117,5 +144,15 @@ public class InsertPlaceActivity extends AppCompatActivity {
         }
 
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == SELECT_IMAGE && null !=data){
+            Uri uri= data.getData();
+            image.setImageURI(uri);
+        }
     }
 }
