9f9a287ba9dc88e17bd5767c0e8bd30445a6e3d3
albertogiura <102516323+albertogiura@users.noreply.github.com>
2023-01-15 01:26:23 +0100

    Aggiornamento recupero campi

--- MyUploadPicApp/app/src/main/java/com/blackbox/myuploadpicapp/MainActivity.java
+++ MyUploadPicApp/app/src/main/java/com/blackbox/myuploadpicapp/MainActivity.java
@@ -22,12 +22,15 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+
 public class MainActivity extends AppCompatActivity {
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     Button uploadButton;
     Button downloadPinButton;
     TextView pinFromDBTextView;
+    TextView pinTitles;
+    TextView pinPositions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +40,8 @@ public class MainActivity extends AppCompatActivity {
         uploadButton = findViewById(R.id.uploadButton);
         downloadPinButton = findViewById(R.id.buttonGetPinFromDB);
         pinFromDBTextView = findViewById(R.id.pinFromDB_textview);
+        pinTitles = findViewById(R.id.pinTitles_textview);
+        pinPositions = findViewById(R.id.pinPos_textview);
 
         uploadButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,13 +74,19 @@ public class MainActivity extends AppCompatActivity {
                         .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                             @Override
                             public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                                String result = "";
+                                String result = "", titles = "", pos = "";
                                 if (task.isSuccessful()) {
                                     for (QueryDocumentSnapshot document : task.getResult()) {
                                         Log.d(TAG, document.getId() + " => " + document.getData());
                                         result += document.getData();
+                                        titles += document.getString("title") + " ";
+                                        //pos += document.get("position");
+
+                                        //firstPinTitleValue.setText(document.get("title").toString());
                                     }
                                     pinFromDBTextView.setText(result);
+                                    pinTitles.setText(titles);
+                                    pinPositions.setText(pos);
                                 } else {
                                     Log.d(TAG, "Error getting documents: ", task.getException());
                                 }
