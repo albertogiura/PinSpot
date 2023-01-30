49317065e2cc29530110d0887edc5e782adf0306
Gambamat <m.gamba12@campus.unimib.it>
2023-01-30 12:58:33 +0100

    Meteo on Pin

--- PinSpot/app/src/main/java/com/blackbox/pinspot/ui/main/PinInfoFragment.java
+++ PinSpot/app/src/main/java/com/blackbox/pinspot/ui/main/PinInfoFragment.java
@@ -2,6 +2,8 @@ package com.blackbox.pinspot.ui.main;
 
 import static android.content.ContentValues.TAG;
 
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -18,9 +20,15 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.blackbox.pinspot.R;
 import com.blackbox.pinspot.databinding.FragmentPinInfoBinding;
+import com.blackbox.pinspot.model.Example;
+import com.blackbox.pinspot.model.Main;
+import com.blackbox.pinspot.model.Weather;
+import com.blackbox.pinspot.model.weatherapi;
+import com.google.android.gms.common.api.Response;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
@@ -30,6 +38,11 @@ import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.firestore.Source;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link PinInfoFragment#newInstance} factory method to
@@ -37,6 +50,11 @@ import com.google.firebase.firestore.Source;
  */
 public class PinInfoFragment extends Fragment {
 
+    String url = "api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}";
+    String apikey = "4f6ec18ab9eb724adb869edca9cbbf63";
+    LocationManager manager;
+    LocationListener locationListener;
+    Double latitude, longitude;
     private FragmentPinInfoBinding fragmentPinInfoBinding;
 
     public PinInfoFragment() {
@@ -80,7 +98,7 @@ public class PinInfoFragment extends Fragment {
 
         String pinID = PinInfoFragmentArgs.fromBundle(getArguments()).getPinID();
         if (pinID != null){
-            fragmentPinInfoBinding.PinTitleTextView.setText(pinID);
+            fragmentPinInfoBinding.loginText.setText(pinID);
 
             FirebaseFirestore db = FirebaseFirestore.getInstance();
 
@@ -88,15 +106,52 @@ public class PinInfoFragment extends Fragment {
             docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
                 @Override
                 public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                    Double latitude, longitude;
                     if (task.isSuccessful()) {
                         DocumentSnapshot document = task.getResult();
                         if (document.exists()) {
                             Log.d(TAG, "DocumentSnapshot data: " + document.getData());
                             latitude = document.getDouble("lat");
                             longitude = document.getDouble("lon");
-                            fragmentPinInfoBinding.PinLatTextView.setText(String.valueOf(latitude));
-                            fragmentPinInfoBinding.pinLongTextView.setText(String.valueOf(longitude));
+                            //fragmentPinInfoBinding.pinLatTextView.setText(String.valueOf(latitude));
+                            //fragmentPinInfoBinding.pinLongTextView.setText(String.valueOf(longitude));
+                            Retrofit retrofit=new Retrofit.Builder()
+                                    .baseUrl("https://api.openweathermap.org/data/2.5/")
+                                    .addConverterFactory(GsonConverterFactory.create())
+                                    .build();
+                            weatherapi myapi=retrofit.create(weatherapi.class);
+                            Call<Example> examplecall = myapi.getweather(Double.parseDouble(String.valueOf(latitude)),Double.parseDouble(String.valueOf(longitude)), apikey);
+                            examplecall.enqueue(new Callback<Example>() {
+                                @Override
+                                public void onResponse(Call<Example> call, retrofit2.Response<Example> response) {
+                                     if (response.code() == 404) {
+                                        Toast.makeText(PinInfoFragment.newInstance().requireContext(), "Please Enter a valid City", Toast.LENGTH_LONG).show();
+                                    } else if (!(response.isSuccessful())) {
+                                        Toast.makeText(PinInfoFragment.newInstance().requireContext(), response.code() + " ", Toast.LENGTH_LONG).show();
+                                        return;
+                                    }
+                                    Example mydata = response.body();
+
+                                    Main main = mydata.getMain();
+                                    Weather[] weather = mydata.getWeather();
+
+
+                                    Double temp = main.getTemp();
+
+                                    String description = weather[0].getDescription();
+                                    String description2 = weather[0].getMain();
+
+                                    Integer temperature = (int) (temp - 273.15);
+                                    fragmentPinInfoBinding.temperatura.setText(String.valueOf(temperature) + " °C");
+                                    fragmentPinInfoBinding.meteo.setText(description);
+                                    // descrizione2.setText(description2);
+                                }
+
+                                @Override
+                                public void onFailure(Call<Example> call, Throwable t) {
+                                    Toast.makeText(PinInfoFragment.newInstance().requireContext(), t.getMessage(),Toast.LENGTH_LONG).show();
+                                }
+
+                            });
                         } else {
                             Log.d(TAG, "No such document");
                         }
@@ -134,7 +189,7 @@ public class PinInfoFragment extends Fragment {
                         }
                     });*/
         } else{
-            fragmentPinInfoBinding.PinTitleTextView.setText("NULL");
+            fragmentPinInfoBinding.loginText.setText("NULL");
         }
     }
 }
