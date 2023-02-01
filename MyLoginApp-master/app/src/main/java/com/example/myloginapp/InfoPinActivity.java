d361e76666acca5c0ff25b8b35438b9d740ff7c7
Gambamat <m.gamba12@campus.unimib.it>
2023-02-01 15:00:52 +0100

    daynight

--- MyLoginApp-master/app/src/main/java/com/example/myloginapp/InfoPinActivity.java
+++ MyLoginApp-master/app/src/main/java/com/example/myloginapp/InfoPinActivity.java
@@ -14,6 +14,6 @@ public class InfoPinActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.infopin);
+        setContentView(R.layout.insertpin);
     }
 }
