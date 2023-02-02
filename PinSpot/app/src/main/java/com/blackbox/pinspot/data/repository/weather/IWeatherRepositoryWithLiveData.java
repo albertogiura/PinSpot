1034a46f1dbcc05eb973b2c9e82e4200fd0da3dd
albertogiura <102516323+albertogiura@users.noreply.github.com>
2023-02-02 22:41:00 +0100

    Eliminazione conflitti

--- PinSpot/app/src/main/java/com/blackbox/pinspot/data/repository/weather/IWeatherRepositoryWithLiveData.java
+++
@@ -1,9 +0,0 @@
-package com.blackbox.pinspot.data.repository.weather;
-
-import androidx.lifecycle.MutableLiveData;
-
-import com.blackbox.pinspot.model.Result;
-
-public interface IWeatherRepositoryWithLiveData {
-    public MutableLiveData<Result> retrieveWeather(Double lat, Double lng);
-}
